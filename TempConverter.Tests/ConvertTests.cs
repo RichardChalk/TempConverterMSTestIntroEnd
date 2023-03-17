29e7bb545d987cad1360fe6da119faf52209e20a
Richard Chalk <barmybrit@gmail.com>
2023-03-17 06:02:23 +0100

    Add project files.

---
+++ TempConverter.Tests/ConvertTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempConverter.Tests
+{
+    [TestClass]
+    public class ConvertTests
+    {
+        private readonly Convert sut;
+        public ConvertTests()
+        {
+            sut = new Convert();
+        }
+
+        [TestMethod]
+        public void Check_F2C()
+        {
+            // Arrange
+            double f = 32;
+            double c = 0;
+
+            // Act
+            double result = sut.f2c(f);
+
+            // Assert
+            // (expected , received)
+            Assert.AreEqual(c, result);
+        }
+
+        [TestMethod]
+        public void Check_C2F()
+        {
+            // Arrange
+            double c = 100;
+            double f = 212;
+
+            // Act
+            double result = sut.c2f(c);
+
+            // Assert
+            // (expected , received)
+            Assert.AreEqual(f, result);
+        }
+
+        [TestMethod]
+        public void Check_K2C()
+        {
+            // Arrange
+            double k = 0;
+            double c = -273.15;
+
+            // Act
+            double result = sut.k2c(k);
+
+            // Assert
+            // (expected , received)
+            Assert.AreEqual(c, result);
+        }
+
+        [TestMethod]
+        public void When_Passing_Under_10_Return_Cold()
+        {
+            // Arrange
+            double c = 9;
+            string tempText = "Kallt";
+
+            // Act
+            string result = sut.c2Text(c);
+
+            // Assert
+            // (expected , received)
+            Assert.AreEqual(tempText, result);
+        }
+
+        [TestMethod]
+        public void When_Passing_Between_10_and_20_Return_Warm()
+        {
+            // Arrange
+            double c = 15;
+            string tempText = "Varmt";
+
+            // Act
+            string result = sut.c2Text(c);
+
+            // Assert
+            // (expected , received)
+            Assert.AreEqual(tempText, result);
+        }
+
+        [TestMethod]
+        public void When_Passing_Over_20_Return_Hot()
+        {
+            // Arrange
+            double c = 25;
+            string tempText = "Hett";
+
+            // Act
+            string result = sut.c2Text(c);
+
+            // Assert
+            // (expected , received)
+            Assert.AreEqual(tempText, result);
+        }
+    }
+}
---
+++ TempConverter/Convert.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempConverter
+{
+    public class Convert
+    {
+        public double f2c(double f)
+        {
+            return (f - 32) * 5 / 9;
+        }
+        public double c2f(double c)
+        {
+            return (c * 9 / 5) + 32;
+        }
+
+        public double k2c(double k)
+        {
+            return (k - 273.15);
+        }
+        public string c2Text(double c)
+        {
+            if (c < 10)
+                return "Kallt";
+            else if (c >= 10 && c <= 20)
+                return "Varmt";
+            return "Hett";
+        }
+    }
+}
